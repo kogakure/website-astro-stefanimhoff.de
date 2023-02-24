140cb697090889090088060b3ddcd65bb3050fab
Stefan Imhoff <stefan@imhoff.name>
2023-02-24 17:01:19 +0100

    feat(svelte): add ThemeBox component

---
+++ src/components/ThemeBox.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	let observer;
+	let isDarkMode = false;
+
+	const checkDarkMode = () => {
+		isDarkMode = document.documentElement.classList.contains('dark');
+	};
+
+	onMount(() => {
+		checkDarkMode();
+
+		observer = new MutationObserver(() => {
+			checkDarkMode();
+		});
+
+		observer.observe(document.documentElement, {
+			attributes: true,
+			attributeFilter: ['class'],
+		});
+	});
+
+	onDestroy(() => {
+		observer.disconnect();
+	});
+</script>
+
+<div class={!isDarkMode && 'hidden'}>
+	<slot name="dark" />
+</div>
+<div class={isDarkMode && 'hidden'}>
+	<slot name="light" />
+</div>
