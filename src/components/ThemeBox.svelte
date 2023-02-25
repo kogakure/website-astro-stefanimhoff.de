21a173409d68af658f0b3a5f36f7c9e2f32932d9
Stefan Imhoff <stefan@imhoff.name>
2023-02-25 22:30:28 +0100

    chore: simplify the ThemeBox component

--- src/components/ThemeBox.svelte
+++ src/components/ThemeBox.svelte
@@ -1,34 +1,6 @@
-<script>
-	import { onDestroy, onMount } from 'svelte';
-
-	let observer;
-	let isDarkMode = false;
-
-	const checkDarkMode = () => {
-		isDarkMode = document.documentElement.classList.contains('dark');
-	};
-
-	onMount(() => {
-		checkDarkMode();
-
-		observer = new MutationObserver(() => {
-			checkDarkMode();
-		});
-
-		observer.observe(document.documentElement, {
-			attributes: true,
-			attributeFilter: ['class'],
-		});
-	});
-
-	onDestroy(() => {
-		observer.disconnect();
-	});
-</script>
-
-<div class={!isDarkMode && 'hidden'}>
+<div class="hidden dark:block">
 	<slot name="dark" />
 </div>
-<div class={isDarkMode && 'hidden'}>
+<div class="block dark:hidden">
 	<slot name="light" />
 </div>
