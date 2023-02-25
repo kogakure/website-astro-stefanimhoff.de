6c15bb55cf9c31a62364c78f3b62662bd827bff5
Stefan Imhoff <stefan@imhoff.name>
2023-02-25 23:27:59 +0100

    chore: update margins of the footer

--- src/components/Subsubheadline.svelte
+++ src/components/Subsubheadline.svelte
@@ -4,7 +4,7 @@
 
 <svelte:element
 	this={as}
-	class={`text-3 font-black tracking-tight mbe-8 mbs-14 first-of-type:mbs-0 dark:font-extrabold ${
+	class={`text-3 font-black tracking-tight mbe-5 mbs-14 first-of-type:mbs-0 dark:font-extrabold ${
 		$$props.class || ''
 	}`}
 >
