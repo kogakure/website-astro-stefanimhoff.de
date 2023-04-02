9b784ae818278f8783204f86c6267ff5a91659b5
Stefan Imhoff <stefan@imhoff.name>
2023-04-02 15:51:09 +0200

    chore(preact): add Preact to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), svelte(), tailwind()],
+  integrations: [mdx(), svelte(), tailwind(), preact()]
 });
