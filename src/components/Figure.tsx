2886aafbf9554dbac32aadfe1de0f5c87b51fd0e
Stefan Imhoff <stefan@imhoff.name>
2023-06-04 11:40:57 +0200

    feat: move essays of 2014

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -17,7 +17,7 @@ export const Figure: FunctionalComponent<Props> = ({
 	...props
 }) => {
 	const classes = cx(
-		'rounded-4 bg-white/50 p-10 mbe-13 mbs-0 mie-0 mis-0 dark:bg-black/80',
+		'rounded-4 bg-white/50 p-8 mbe-13 mbs-0 mie-0 mis-0 dark:bg-black/80',
 		{ 'figure-wide': size === 'wide', 'figure-fullsize': size === 'fullsize' },
 		className
 	);
