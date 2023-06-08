277f191114a40dd28b53dc0ffba38eb8b3966afe
Stefan Imhoff <stefan@imhoff.name>
2023-06-08 19:27:05 +0200

    feat: add Tag pages and tags to journal pages

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,20 +2,43 @@ import cx from 'classnames';
 
 import type { ComponentChild, FunctionalComponent } from 'preact';
 
+import { Link } from '.';
+
 interface Props {
-	class?: string;
+	active?: boolean;
 	children: ComponentChild;
+	class?: string;
+	href?: string;
 }
 
-export const Tag: FunctionalComponent<Props> = ({ class: className, children, ...props }) => {
+export const Tag: FunctionalComponent<Props> = ({
+	active,
+	class: className,
+	children,
+	href,
+	...props
+}) => {
 	const classes = cx(
-		'inline-flex items-center justify-center rounded-25 border-2 border-shibui-350 bg-shibui-200 px-3 py-1 text-1 leading-none text-black mie-1 dark:border-shibui-750 dark:bg-shibui-950 dark:text-white',
+		'capitalize border-3 relative inline-flex items-center justify-center rounded-25 border-2 border-shibui-350 bg-shibui-200 px-3 py-1 text-1 font-normal leading-none text-black mie-1 pbs-3 dark:border-shibui-750 dark:bg-shibui-950 dark:text-white min-w-[50px]',
+		{
+			'!bg-shibui-500 !text-white hover:!bg-shibui-700 dark:!border-shibui-500 dark:!bg-shibui-700 hover:dark:!bg-shibui-400':
+				href,
+			'active [&.active]:!border-black/25 !text-white [&.active]:!bg-accent': active,
+		},
 		className
 	);
 
 	return (
-		<span class={classes} {...props}>
-			{children}
-		</span>
+		<>
+			{href ? (
+				<Link href={href} class={classes} {...props}>
+					{children}
+				</Link>
+			) : (
+				<span class={classes} {...props}>
+					{children}
+				</span>
+			)}
+		</>
 	);
 };
