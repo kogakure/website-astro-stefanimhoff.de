d2a9d9193c6611ba5d37b0840c075f5db8fe395a
Stefan Imhoff <stefan@imhoff.name>
2023-06-09 18:35:09 +0200

    feat: add helper to sort journal posts

---
+++ src/utils/format-posts.ts
@@ -0,0 +1,55 @@
+import { sortByAlphabet, sortByDate } from '.';
+
+import type { CollectionEntry } from 'astro:content';
+
+export const formatPosts = (
+	posts: CollectionEntry<'journal'>[],
+	{
+		removeDrafts = true,
+		removeFuture = true,
+		sortBy = 'date',
+		sortOrder = 'desc',
+		limit,
+	}: {
+		removeDrafts?: boolean;
+		removeFuture?: boolean;
+		sortBy?: 'date' | 'alphabet' | 'random';
+		sortOrder?: 'asc' | 'desc';
+		limit?: number;
+	}
+): CollectionEntry<'journal'>[] => {
+	const filteredPosts = posts.reduce((acc: CollectionEntry<'journal'>[], post) => {
+		const { date, draft } = post.data;
+
+		// Remove draft content
+		if (removeDrafts && draft) return acc;
+
+		// Remove future content
+		if (removeFuture && new Date(date) > new Date()) return acc;
+
+		acc.push(post);
+
+		return acc;
+	}, []);
+
+	// Sort posts
+	if (sortBy === 'date') {
+		filteredPosts.sort(sortByDate);
+	} else if (sortBy === 'alphabet') {
+		filteredPosts.sort(sortByAlphabet);
+	} else {
+		filteredPosts.sort(() => Math.random() - 0.5);
+	}
+
+	// Sort order
+	if (sortOrder === 'asc') {
+		filteredPosts.reverse();
+	}
+
+	// Limit number of posts
+	if (typeof limit === 'number') {
+		return filteredPosts.slice(0, limit);
+	}
+
+	return filteredPosts;
+};
---
+++ src/utils/sort-by-alphabet.ts
@@ -0,0 +1,3 @@
+export const sortByAlphabet = (a: any, b: any) => {
+	return a.data.title.localeCompare(b.data.title);
+};
