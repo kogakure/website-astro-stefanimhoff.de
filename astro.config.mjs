bb32c6494c892bff54e4fb2b6bcbd84035de70a9
Stefan Imhoff <stefan@imhoff.name>
2023-06-10 19:25:36 +0200

    feat: add prefetch for pagination pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,22 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import { astroImageTools } from 'astro-imagetools';
 import { remarkReadingTime, remarkWidont } from './src/utils';
-
 import { defineConfig } from 'astro/config';
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		shikiConfig: {
-			theme: 'nord',
-			langs: [],
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({
-			remarkPlugins: [remarkReadingTime, remarkWidont],
-		}),
-		tailwind(),
-		preact({ compat: true }),
-		astroImageTools,
-	],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+      langs: [],
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    remarkPlugins: [remarkReadingTime, remarkWidont]
+  }), tailwind(), preact({
+    compat: true
+  }), astroImageTools, prefetch()]
 });
