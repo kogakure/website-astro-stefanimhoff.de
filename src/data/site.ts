f6205c1e4ab26f4d3e442311580c1742ebb2c33d
Stefan Imhoff <stefan@imhoff.name>
2023-06-10 20:31:47 +0200

    feat: add Sal.js scroll animations

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,3 +1,25 @@
+const animationType = 'slide-up';
+const delay = 300;
+const duration = 800;
+const easing = 'ease-out-sine';
+
 export const site = {
+	title: 'Stefan Imhoff',
+	description: 'Front-End Web Developer from Hamburg, Germany',
+	url: 'https://www.stefanimhoff.de',
 	author: 'Stefan Imhoff',
+	tagline: 'Front-End Web Developer • Designer • Minimalist • Japanophile',
+	faviconPath: '/assets/images/branding/favicons/',
+	twitter: '@kogakure',
+};
+
+export const animation = {
+	'data-sal': animationType,
+	'data-sal-duration': duration,
+	'data-sal-easing': easing,
+};
+
+export const animationDelay = {
+	...animation,
+	'data-sal-delay': delay,
 };
