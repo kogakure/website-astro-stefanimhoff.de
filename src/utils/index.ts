45bbc98268252e460949534c83aabd55a2614b4b
Stefan Imhoff <stefan@imhoff.name>
2023-06-13 12:39:03 +0200

    feat: add isProduction function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './date';
 export * from './format-posts';
+export * from './is-production';
 export * from './pick-two-random-colors';
 export * from './remark-reading-time';
 export * from './remark-widont';
