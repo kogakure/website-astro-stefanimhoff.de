eac9efd4f3f453f72a917bf964fcea173e506b3f
Stefan Imhoff <stefan@imhoff.name>
2023-06-13 10:19:34 +0200

    feat: add manifest.webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { astroImageTools } from 'astro-imagetools';
+import webmanifest from 'astro-webmanifest';
 import { defineConfig } from 'astro/config';
+
+import { site } from './src/data/site';
 import { remarkReadingTime, remarkWidont } from './src/utils';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://www.stefanimhoff.de',
 	markdown: {
@@ -31,6 +33,27 @@ export default defineConfig({
 			filter: (page) =>
 				page !== 'https://www.stefanimhoff.de/cv/' &&
 				page !== 'https://www.stefanimhoff.de/imprint/',
+		}),
+		webmanifest({
+			name: site.title,
+			icons: [
+				{
+					src: '/assets/images/branding/favicons/favicon-192.png',
+					sizes: '192x192',
+					type: 'image/png',
+				},
+				{
+					src: '/assets/images/branding/favicons/favicon-512.png',
+					sizes: '512x512',
+					type: 'image/png',
+				},
+			],
+			short_name: site.title,
+			description: site.description,
+			start_url: '/',
+			theme_color: '#e7e6e4',
+			background_color: '#e7e6e4',
+			display: 'standalone',
 		}),
 	],
 });
