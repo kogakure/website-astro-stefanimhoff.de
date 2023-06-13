f3986f9d6c7461d8bdd559604b5bf13b8f538b67
Stefan Imhoff <stefan@imhoff.name>
2023-06-13 18:52:13 +0200

    feat: implement hack for journal post RSS feed
    Astro is currently not able to render the compiled HTML of MDX files to a string.
    This makes it impossible to render the content for an RSS feed.
    
    Issue: Container API: render components in isolation
    https://github.com/withastro/roadmap/issues/533
    
    Proposal: MDX compiledContent() support
    https://github.com/withastro/roadmap/discussions/419
    
    To still be able to have full content for RSS feeds, this dirty hack,
    Scott Willsey writes about in his 2-part blog post is needed:
    
    https://scottwillsey.com/rss-pt1/
    https://scottwillsey.com/rss-pt2/

--- src/utils/sort-by-date.ts
+++ src/utils/sort-by-date.ts
@@ -1,3 +1,7 @@
 export const sortByDate = (a: any, b: any) => {
 	return Date.parse(b.data.date.toISOString()) - Date.parse(a.data.date.toISOString());
 };
+
+export const sortMarkdownByDate = (a: any, b: any) => {
+	return Date.parse(b.frontmatter.date) - Date.parse(a.frontmatter.date);
+};
