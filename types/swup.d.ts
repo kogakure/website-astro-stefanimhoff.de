7f2470157d67052c56d6447e56fb4c5e2929555a
Stefan Imhoff <stefan@imhoff.name>
2023-06-14 15:55:53 +0200

    feat: add page transitions with Swup

---
+++ types/swup.d.ts
@@ -0,0 +1,5 @@
+declare module '@swup/fade-theme';
+declare module '@swup/head-plugin';
+declare module '@swup/a11y-plugin';
+declare module '@swup/preload-plugin';
+declare module '@swup/scroll-plugin';
