be20aeb5ea6d06431cd30a190711136f25086eb7
Stefan Imhoff <stefan@imhoff.name>
2024-03-12 20:46:34 +0100

    chore: remove duplicate class names

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Bookshelf: FunctionalComponent<Props> = ({ children, ...props }) => {
 	return (
 		<article
-			class="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] place-items-center justify-center gap-[20px] rounded-4 bg-white/50 p-10 mbe-10 mbe-13 mbs-0 mbs-0 mie-0 mis-0 dark:bg-black/80"
+			class="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] place-items-center justify-center gap-[20px] rounded-4 bg-white/50 p-10 mbe-13 mbs-0 mie-0 mis-0 dark:bg-black/80"
 			{...props}
 		>
 			{children}
