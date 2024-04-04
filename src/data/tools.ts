ae280fe2c7bb8106986250e72646cca4a7496fcb
Stefan Imhoff <stefan@imhoff.name>
2024-04-04 16:04:10 +0200

    feat: convert all images to WebP

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -8,27 +8,27 @@ export const development = [
 	{
 		name: 'Neovim',
 		url: 'https://neovim.io/',
-		image: '/assets/images/tools/webp/neovim.webp',
+		image: '/assets/images/tools/neovim.webp',
 	},
 	{
 		name: 'Tmux',
 		url: 'https://tmux.github.io/',
-		image: '/assets/images/tools/webp/tmux.webp',
+		image: '/assets/images/tools/tmux.webp',
 	},
 	{
 		name: 'Wezterm',
 		url: 'https://wezfurlong.org/wezterm/',
-		image: '/assets/images/tools/webp/wezterm.webp',
+		image: '/assets/images/tools/wezterm.webp',
 	},
 	{
 		name: 'Visual Studio Code',
 		url: 'https://code.visualstudio.com/',
-		image: '/assets/images/tools/webp/visual-studio-code.webp',
+		image: '/assets/images/tools/visual-studio-code.webp',
 	},
 	{
 		name: 'Kaleidoscope',
 		url: 'https://kaleidoscope.app/',
-		image: '/assets/images/tools/webp/kaleidoscope.webp',
+		image: '/assets/images/tools/kaleidoscope.webp',
 	},
 ];
 
@@ -36,32 +36,32 @@ export const design = [
 	{
 		name: 'Affinity Designer',
 		url: 'https://affinity.serif.com/designer/',
-		image: '/assets/images/tools/webp/affinity-designer.webp',
+		image: '/assets/images/tools/affinity-designer.webp',
 	},
 	{
 		name: 'Affinity Photo',
 		url: 'https://affinity.serif.com/photo/',
-		image: '/assets/images/tools/webp/affinity-photo.webp',
+		image: '/assets/images/tools/affinity-photo.webp',
 	},
 	{
 		name: 'Affinity Publisher',
 		url: 'https://affinity.serif.com/publisher/',
-		image: '/assets/images/tools/webp/affinity-publisher.webp',
+		image: '/assets/images/tools/affinity-publisher.webp',
 	},
 	{
 		name: 'Eagle',
 		url: 'https://eagle.cool/',
-		image: '/assets/images/tools/webp/eagle.webp',
+		image: '/assets/images/tools/eagle.webp',
 	},
 	{
 		name: 'Procreate',
 		url: 'https://procreate.com/',
-		image: '/assets/images/tools/webp/procreate.webp',
+		image: '/assets/images/tools/procreate.webp',
 	},
 	{
 		name: 'Typeface',
 		url: 'https://typefaceapp.com/',
-		image: '/assets/images/tools/webp/typeface.webp',
+		image: '/assets/images/tools/typeface.webp',
 	},
 ];
 
@@ -69,57 +69,57 @@ export const research = [
 	{
 		name: 'iA Writer',
 		url: 'https://ia.net/writer',
-		image: '/assets/images/tools/webp/ia-writer.webp',
+		image: '/assets/images/tools/ia-writer.webp',
 	},
 	{
 		name: 'iA Presenter',
 		url: 'https://ia.net/presenter',
-		image: '/assets/images/tools/webp/ia-presenter.webp',
+		image: '/assets/images/tools/ia-presenter.webp',
 	},
 	{
 		name: 'Obsidian',
 		url: 'https://obsidian.md/',
-		image: '/assets/images/tools/webp/obsidian.webp',
+		image: '/assets/images/tools/obsidian.webp',
 	},
 	{
 		name: 'DEVONthink',
 		url: 'https://www.devontechnologies.com/apps/devonthink',
-		image: '/assets/images/tools/webp/devonthink.webp',
+		image: '/assets/images/tools/devonthink.webp',
 	},
 	{
 		name: 'Raindrop.io',
 		url: 'https://raindrop.io/',
-		image: '/assets/images/tools/webp/raindrop.io.webp',
+		image: '/assets/images/tools/raindrop.io.webp',
 	},
 	{
 		name: 'Reeder',
 		url: 'https://reeder.app/',
-		image: '/assets/images/tools/webp/reeder.webp',
+		image: '/assets/images/tools/reeder.webp',
 	},
 	{
 		name: 'Readwise Reader',
 		url: 'https://readwise.io/read',
-		image: '/assets/images/tools/webp/readwise-reader.webp',
+		image: '/assets/images/tools/readwise-reader.webp',
 	},
 	{
 		name: 'Readwise',
 		url: 'https://readwise.io/i/stefan805',
-		image: '/assets/images/tools/webp/readwise.webp',
+		image: '/assets/images/tools/readwise.webp',
 	},
 	{
 		name: 'Snipd',
 		url: 'https://www.snipd.com/',
-		image: '/assets/images/tools/webp/snipd.webp',
+		image: '/assets/images/tools/snipd.webp',
 	},
 	{
 		name: 'MindNode',
 		url: 'https://www.mindnode.com/',
-		image: '/assets/images/tools/webp/mindnode.webp',
+		image: '/assets/images/tools/mindnode.webp',
 	},
 	{
 		name: 'BibDesk',
 		url: 'https://bibdesk.sourceforge.io/',
-		image: '/assets/images/tools/webp/bibdesk.webp',
+		image: '/assets/images/tools/bibdesk.webp',
 	},
 ];
 
@@ -127,46 +127,46 @@ export const other = [
 	{
 		name: 'Things',
 		url: 'https://culturedcode.com/things/',
-		image: '/assets/images/tools/webp/things.webp',
+		image: '/assets/images/tools/things.webp',
 	},
 	{
 		name: 'Raycast',
 		url: 'https://www.raycast.com/hey/059d78ca',
-		image: '/assets/images/tools/webp/raycast.webp',
+		image: '/assets/images/tools/raycast.webp',
 	},
 	{
 		name: 'Arc',
 		url: 'https://arc.net/gift/3a2fd063',
-		image: '/assets/images/tools/webp/arc-browser.webp',
+		image: '/assets/images/tools/arc-browser.webp',
 	},
 	{
 		name: 'Brave Browser',
 		url: 'https://brave.com/',
-		image: '/assets/images/tools/webp/brave-browser.webp',
+		image: '/assets/images/tools/brave-browser.webp',
 	},
 	{
 		name: 'Proton Mail',
 		url: 'https://proton.me/mail',
-		image: '/assets/images/tools/webp/protonmail.webp',
+		image: '/assets/images/tools/protonmail.webp',
 	},
 	{
 		name: 'Proton VPN',
 		url: 'https://protonvpn.com/',
-		image: '/assets/images/tools/webp/protonvpn.webp',
+		image: '/assets/images/tools/protonvpn.webp',
 	},
 	{
 		name: 'Proton Pass',
 		url: 'https://proton.me/pass',
-		image: '/assets/images/tools/webp/protonpass.webp',
+		image: '/assets/images/tools/protonpass.webp',
 	},
 	{
 		name: 'Yoink',
 		url: 'https://eternalstorms.at/yoink/mac/',
-		image: '/assets/images/tools/webp/yoink.webp',
+		image: '/assets/images/tools/yoink.webp',
 	},
 	{
 		name: 'Karabiner-Elements',
 		url: 'https://karabiner-elements.pqrs.org/',
-		image: '/assets/images/tools/webp/karabiner-elements.webp',
+		image: '/assets/images/tools/karabiner-elements.webp',
 	},
 ];
