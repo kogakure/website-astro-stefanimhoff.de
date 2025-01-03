1560013a07617582d3c6a9d1a545ccf4842ae587
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-01-03 13:39:25 +0100

    feat: remove Sal.js
    Fewer animations for the new year.

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,3 @@
-const animationType = 'slide-up';
-const delay = 300;
-const duration = 800;
-const easing = 'ease-out-sine';
-
 export const site = {
 	title: 'Stefan Imhoff',
 	description: 'Web UI Engineer from Hamburg, Germany',
@@ -11,15 +6,4 @@ export const site = {
 	tagline: 'Web UI Engineer • Minimalist • Stoic • Libertarian • Japanophile',
 	faviconPath: '/assets/images/branding/favicons/',
 	x: '@kogakure',
-};
-
-export const animation = {
-	'data-sal': animationType,
-	'data-sal-duration': duration,
-	'data-sal-easing': easing,
-};
-
-export const animationDelay = {
-	...animation,
-	'data-sal-delay': delay,
 };
