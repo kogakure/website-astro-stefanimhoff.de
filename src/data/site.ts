aae33d65ab33cdc86b51daf222931606655e4c93
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-07-19 16:47:34 +0200

    chore: update job title

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,9 +1,9 @@
 export const site = {
 	title: 'Stefan Imhoff',
-	description: 'Web UI Engineer from Hamburg, Germany',
+	description: 'Web Developer from Hamburg, Germany',
 	url: 'https://www.stefanimhoff.de',
 	author: 'Stefan Imhoff',
-	tagline: '👨🏻‍💻 Web UI Engineer • ⬜️ Minimalist • 🏛️ Stoic • 🏴 Ancap • ⛩️ Japanophile',
+	tagline: '👨🏻‍💻 Web Developer • ⬜️ Minimalist • 🏛️ Stoic • 🏴 Ancap • ⛩️ Japanophile',
 	faviconPath: '/assets/images/branding/favicons/',
 	x: '@kogakure',
 };
