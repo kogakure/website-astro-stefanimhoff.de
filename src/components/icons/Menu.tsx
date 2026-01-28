7ca011742cc34022415a795a64c2e6b5c23cb40b
Stefan Imhoff <gpg@kogakure.8shield.net>
2026-01-28 15:22:24 +0100

    feat: add new logo, new theme switcher and realign navigation items

---
+++ src/components/icons/Menu.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+const Menu = (props: SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" {...props}>
+		<path d="M3 4h18v2H3zm0 7h18v2H3zm0 7h18v2H3z" />
+	</svg>
+);
+export default Menu;
