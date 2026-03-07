c3e1b0a99de71bfa09864f7d9215ce8ea0698ee2
Stefan Imhoff <gpg@kogakure.8shield.net>
2026-03-07 12:18:00 +0100

    feat: update title

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,9 +1,9 @@
 export const site = {
 	title: 'Stefan Imhoff',
-	description: 'Web Developer from Hamburg, Germany',
+	description: 'Web Design Engineer from Hamburg, Germany',
 	url: 'https://www.stefanimhoff.de',
 	author: 'Stefan Imhoff',
-	tagline: '👨🏻‍💻 Web Developer • ⬜️ Minimalist • 🏛️ Stoic • 🏴 Ancap • ⛩️ Japanophile',
+	tagline: '👨🏻‍💻 Design Engineer • ⬜️ Minimalist • 🏛️ Stoic • 🏴 Ancap • ⛩️ Japanophile',
 	faviconPath: '/assets/images/favicons/',
 	x: '@kogakure',
 };
